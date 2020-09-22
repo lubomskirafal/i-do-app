a2ab054d79991e141937efa0ed263e7bc1f1f734
lubomskirafal <lubomski.rafal@gmail.com>
2020-09-22 17:05:40 +0200

    Updates

---
+++ precache-manifest.cf91f727359c822710838e89eddb772f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30ddf7b22008fdaf3f00debfe4dc8074",
+    "url": "/i-do-app/index.html"
+  },
+  {
+    "revision": "53608aeda52a1eb8f8f5",
+    "url": "/i-do-app/static/css/main.0d47f56b.chunk.css"
+  },
+  {
+    "revision": "fbc23848f85031d7fc6e",
+    "url": "/i-do-app/static/js/2.6018ffe0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/i-do-app/static/js/2.6018ffe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53608aeda52a1eb8f8f5",
+    "url": "/i-do-app/static/js/main.80a738b2.chunk.js"
+  },
+  {
+    "revision": "19a34b751d0fb4ec8791",
+    "url": "/i-do-app/static/js/runtime-main.c802a76e.js"
+  }
+]);
